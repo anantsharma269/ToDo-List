a35586b49bd801a59c9dd5dce80658eecb0e3fc8
Anant sharma <anant7551@gmail.com>
2024-09-13 08:15:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(){
+    var inputEle = document.getElementById('inp').value;
+
+    var ele = document.createElement('li' );
+
+     ele.innerText = inputEle;
+    
+    document.getElementById('display').appendChild(ele);
+
+    document.getElementById('inp').value="";
+
+    deleteEle(ele);
+
+} 
+
+function deleteEle(ele){
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = "Delete";
+    ele.appendChild(deleteBtn);
+    deleteBtn.onclick = function(){
+        ele.remove();
+    }
+
+}
